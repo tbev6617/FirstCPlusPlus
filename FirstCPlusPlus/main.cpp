95872b5b7a4693e973a80c44fe5c26be178640eb
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-01-23 08:49:48 -0700

    Made my project say Hello World

---
+++ FirstCPlusPlus/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  FirstCPlusPlus
+//
+//  Created by Bevan, Tyler on 1/23/18.
+//  Copyright © 2018 Bevan, Tyler. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Hello, World! What is your name?" << endl;
+    string word;
+    cin >> word;
+    cout << "Hello " << word << endl;
+    
+    
+    return 0;
+}
